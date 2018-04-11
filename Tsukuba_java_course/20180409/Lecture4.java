bc73beda0ae77529a3f52cc9949ab5b79777a317
JunyaUSUI <junya.usui.0714@gmail.com>
2018-04-11 18:10:39 +0900

    no message

---
+++ Tsukuba_java_course/20180409/Lecture4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner(System.in);
+import static java.lang.System.*;
+
+/*
+継承：すでにあるクラスの機能を新しいクラスが引き継ぐこと。拡張が容易にできる。
+
+
+*/
+class Point {
+    int x;
+    int y;
+
+    void printInfo() {
+	out.println("print");
+    }
+}
+
+class Lecture4 {
+    public static void main(String[] agrs) {
+	
+    }
+}
+
---
+++ Tsukuba_java_course/20180409/Sample11_1.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+import java.util.Scanner(System.in);
+
+/*
+クラスの継承
+*/
+
+class Car {
+    private int num;
+    private double gas;
+}
+
+class RecingCar extends Car {
+    
+}
