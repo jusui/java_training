e4226355c1665ade972519a8720e8afcf439cad1
JunyaUSUI <junya.usui.0714@gmail.com>
2018-05-07 23:35:41 +0900

    no message

---
+++ Algorithm/BubbleSort.java
@@ -0,0 +1,43 @@
+import static java.lang.System.*;
+import java.io.*;
+
+public class BubbleSort {
+    public static void main(String[] args) throws IOException {
+
+	out.println("入力する配列の要素数 num を入力して下さい。");
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String str = br.readLine();
+	int num = Integer.parseInt(str);
+	int[] data = new int[num];
+	// int[] data = { 4, 6, 7, 9, 2 };
+	
+	out.println("入力する配列の要素を入力して下さい。");
+	
+	for ( int i = 0; i < num; i++ ) {
+	    str = br.readLine();
+	    int tmp = Integer.parseInt(str);
+	    data[i] = tmp;
+	}
+
+	sort(data);
+	out.println("\n Now sorting... \n");
+	for ( int element : data ) {
+	    out.println(element + " ");
+	}
+    }
+
+    public static void sort(int[] data) {
+	for ( int i = data.length - 1; i > 0; i-- ) { // dec
+	    for ( int j = 0; j < i; j++ ) {
+		if ( data[j] > data[j + 1] ) {
+		    // 隣接要素の交換
+		    int tmp = data[j + 1];
+		    data[j + 1] = data[j];
+		    data[j] = tmp;
+		}
+	    }
+	}
+    }
+    
+}
