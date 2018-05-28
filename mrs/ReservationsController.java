7a6dde7bbd336631aa5a33bbfcf50b18443ebcc3
JunyaUSUI <junya.usui.0714@gmail.com>
2018-05-28 22:03:37 +0900

    会議室予約システム

--- mrs/ReservationsController.java
+++ mrs/ReservationsController.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -47,78 +47,60 @@ public class ReservationsController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	String reserveForm(
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
+	String reserveForm(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
 			@PathVariable("roomId") Integer roomId, Model model) {
 		ReservableRoomId reservableRoomId = new ReservableRoomId(roomId, date);
 		List<Reservation> reservations = reservationService.findReservations(reservableRoomId);
-
-		LocalTime baseTime = LocalTime.of(0, 0);
-		// Make a LocalDate object 30/min
-		// for 認証ユーザ
-		List<LocalTime> timeList = IntStream.range(0, 24 * 2)
-				.mapToObj(i -> baseTime.plusMinutes(30 * i)).collect(Collectors.toList());
+		List<LocalTime> timeList = Stream.iterate(LocalTime.of(0, 0), t -> t.plusMinutes(30)).limit(24 * 2)
+				.collect(Collectors.toList());
 		model.addAttribute("room", roomService.findMeetingRoom(roomId));
 		model.addAttribute("reservations", reservations);
 		model.addAttribute("timeList", timeList);
-		// model.addAttribute("user", dummyUser());
 		return "reservation/reserveForm";
 	}
 
-	//
-	// 予約処理：reserve()
-	//
-	// (1)予約処理：Text
+//	private User dummyUser() {
+//		User user = new User();
+//		user.setUserId("taro-yamada");
+//		user.setFirstName("太郎");
+//		user.setLastName("山田");
+//		user.setRoleName(RoleName.USER);
+//		return user;
+//	}
+
 	@RequestMapping(method = RequestMethod.POST)
 	String reserve(@Validated ReservationForm form, BindingResult bindingResult,
 			@AuthenticationPrincipal ReservationUserDetails userDetails,
 			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
 			@PathVariable("roomId") Integer roomId, Model model) {
-		// 入力チェックでエラーの有無を確認, エラーならフォーム表示画面へ遷移
-		if ( bindingResult.hasErrors() ) {
+		if (bindingResult.hasErrors()) {
 			return reserveForm(date, roomId, model);
 		}
-
-		// def reservableRoom
 		ReservableRoom reservableRoom = new ReservableRoom(new ReservableRoomId(roomId, date));
 		Reservation reservation = new Reservation();
 		reservation.setStartTime(form.getStartTime());
 		reservation.setEndTime(form.getEndTime());
 		reservation.setReservableRoom(reservableRoom);
-		//reservation.setUser(dummyUser());
 		reservation.setUser(userDetails.getUser());
-
 		try {
 			reservationService.reserve(reservation);
-		} catch(UnavailableReservationException | AlreadyReservedException e )  {
-			// 例外処理：予約失敗メッセージ
+		} catch (UnavailableReservationException | AlreadyReservedException e) {
 			model.addAttribute("error", e.getMessage());
 			return reserveForm(date, roomId, model);
 		}
-		// 予約成功：予約一覧表示画面へリダイレクト
 		return "redirect:/reservations/{date}/{roomId}";
 	}
 
-	//
-	// 予約取り消し：cancel()
-	//
 	@RequestMapping(method = RequestMethod.POST, params = "cancel")
-	String cancel(@RequestParam("reservationId") Integer reservationId,
-			@PathVariable("roomId") Integer roomId,
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
-			Model model) {
-
-		// ReservationUserDetailsから認証済のUserオブジェクトを取得し，予約処理に使用
-		// User user = userDetails.getUser();
+	String cancel(@RequestParam("reservationId") Integer reservationId, @PathVariable("roomId") Integer roomId,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date, Model model) {
 		try {
 			Reservation reservation = reservationService.findOne(reservationId);
 			reservationService.cancel(reservation);
-			// ハンドリングする例外を org.springframework.security.access.AccessDeniedExceptinonに変更
 		} catch (AccessDeniedException e) {
 			model.addAttribute("error", e.getMessage());
 			return reserveForm(date, roomId, model);
 		}
 		return "redirect:/reservations/{date}/{roomId}";
-
 	}
 }
