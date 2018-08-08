5ce45069642c1b074c62b348a4091f8d01b65e32
JunyaUSUI <junya.usui.0714@gmail.com>
2018-08-09 00:07:52 +0900

    add permutation method

--- DataStructure/DS_1_2.java
+++ DataStructure/DS_1_2.java
@@ -8,13 +8,25 @@ class DS_1_2 {
     public static String sort(String s) {
 	char[] content = s.toCharArray();
 	Arrays.sort(content);
+	return new String(content);
+    }
 
+    public static boolean permutation(String s, String t) {
+	if ( s.length() != t.length() ) {
+	    return false;
+	}
+	return sort(s).equals(sort(t));
+    }
+    
     public static void main(String[] args) {
 	
-	Scanner scan = new Scanner(System.in);
-	System.out.print("Input string > ");
-	String str = scan.next();
-	System.out.println(isUniqueChars(str));
+	Scanner scan1 = new Scanner(System.in);
+	Scanner scan2 = new Scanner(System.in);	
+	System.out.print("Input string 1 > ");
+	String str1 = scan1.next();
+	System.out.print("Input string 2 > ");	
+	String str2 = scan2.next();	
+	System.out.println(permutation(str1, str2));
 
     }
 }
